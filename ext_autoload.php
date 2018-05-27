d3c54b3772b88034717e4153328d5900bf4d559b
Franz Holzinger <holzinger@ttproducts.de>
2018-05-27 19:45:32 +0200

    * restructuring for TYPO3 8.7.13: inc/class.tx_watchwords_tslibfe.php -> Classes/Hooks/PageCacheHook.php pi1/class.tx_watchwords_pi1_wizicon.php -> Classes/Hooks/WizardIcon.php pi1/class.tx_watchwords_pi1.php -> Classes/Plugin/Watchwords.php ext_typoscript_constants.txt -> Configuration/TypoScript/PluginSetup/constants.txt ext_typoscript_setup.txt -> Configuration/TypoScript/PluginSetup/setup.txt pi1/template_biblegateway.inc -> Resources/Private/Templates/template_biblegateway.html pi1/ce_wiz.gif -> Resources/Public/Icons/watchwords.gif

--- ext_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$key = 'watchwords';
-$extensionPath = t3lib_extMgm::extPath($key, $script);
-
-return array(
-	'tx_ttproducts_wizicon' => $extensionPath . 'class.tx_ttproducts_wizicon.php',
-);
-?>
