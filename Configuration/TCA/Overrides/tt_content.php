ba714d82a7cecc6cd6f12c6f52b3284abf2d56e5
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-02 16:01:15 +0200

    syntax

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,23 +2,22 @@
 
 call_user_func(function($extensionKey, $table)
 {
-        $listType = 'watchwords_watch';
+    $listType = 'watchwords_watch';
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'Watchwords',
-            'Watch',
-            'The Watchword List',
-            'EXT:watchwords/Resources/Public/Icons/Extension.svg'
-        );
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'Watchwords',
+        'Watch',
+        'The Watchword List',
+        'EXT:watchwords/Resources/Public/Icons/Extension.svg'
+    );
 
-        $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-            $listType,
-            'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/Watch.xml'
-        );
-        $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] =
-            'recursive,pages';
-    }
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        $listType,
+        'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/Watch.xml'
+    );
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] =
+        'recursive,pages';
 }, 'watchwords', basename(__FILE__, '.php'));
 
 
